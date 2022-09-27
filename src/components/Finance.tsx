e58b6bf5be030bc70bac24c38038491a13c2e056
esraevren <dytesraevrenn@gmail.com>
2022-09-27 15:02:01 +0300

    finance api problem solved

--- src/components/Finance.tsx
+++ src/components/Finance.tsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 function Finance() {
   useEffect(() => {
     const getFinance = async () => {
-      const response = await axios.get(
-        'https://api.collectapi.com/weather/getWeather?data.city=ankara', 
+      const response = await axios.get('https://api.collectapi.com/economy/allCurrency'
+        , {
+       headers:{
+        "Authorization": "apikey 1z2myxRiLOzI2iFjtipjAb:5IEGuIKvzg7AspVuVCKt0w"
+       },
+       
+           }
   );
       console.log(response.data);
       
